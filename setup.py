2273f2cbcceb0fc090b47d04303d501c24a5a4d8
Shubham Jha <sjha0090@gmail.com>
2022-01-18 18:29:25 +0530

    classifiers added

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
+import os
 from setuptools import setup
 
-setup(name='getconti',
+setup(name='getcontinents',
       version='0.1.0',
       description='This module can help you to get the continent of different countries',
       url='https://github.com/meShubhamJha/getconti',
@@ -8,5 +9,10 @@ setup(name='getconti',
       author_email='sjha0090@gmail.com',
       license='MIT',
       packages=['getconti'],
+      python_requires='>3.5.2',
       install_requires=['pandas',"os"],
+      classifiers = [
+      "Programming Language :: Python :: 3",
+      "Licence :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent"]
       zip_safe=False)
