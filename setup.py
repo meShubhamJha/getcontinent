6a2362edad3d1b120ce4182ebd7cfa71d5b3bef4
Shubham Jha <sjha0090@gmail.com>
2022-01-18 15:08:33 +0530

    Version 0.1.0

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='getconti',
+      version='0.1.0',
+      description='This module can help you to get the continent of different countries',
+      url='https://github.com/meShubhamJha/getconti',
+      author='Shubham Jha',
+      author_email='sjha0090@gmail.com',
+      license='MIT',
+      packages=['getconti'],
+      requires=['pandas',"os"],
+      zip_safe=False)
